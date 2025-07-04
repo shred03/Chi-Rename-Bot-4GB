16f09ea9dc3499a4d44c20351c7eeaaf76196be8
shred03 <mithunyada12345@gmail.com>
2025-07-04 15:11:48 +0530

    removed secrets

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", "25349731"))
-API_HASH = os.environ.get("API_HASH", "58c4653460a25b1f55d22761337b5781")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8131782044:AAF0Vbrk4Ozcx4MqgNoLZ5dL02oBhy6l05A")
-ADMIN = int(os.environ.get("ADMIN", "7942723471"))
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "BQGCzmMAZJ6iU4ud06JTurf86W9vA-O_vQsGBLt0jO8mgFF-e0usBZVL9dQ9C2Q36rdAYsLWJCpdLbnvZmxZzuumDyo-lQ1V-pgDtnL4ZTsewohfCL0ZKgyIzMk4WL_riN5lJPtniM1X8YkFlkfqB6yLq5brFjkWC1hfLx9TudE3N7KfIodnWrr9Q9GlTfyh61g1OKWuj_cxQIJ3Gw0CgKAVi9hPCaa0D_-0rmyjn6HGZk-6ExNl0rEFXwsHtXFL2UEIyPoh4OwlSqIeYR6GZSLbqpG2SCUf9L4-0YyIorf4Sxst5Q0kvlyt8yVOajddlY7vmvKlrsmV9BlFrTQ_b9xpKu1IBgAAAAHZbFePAA")
+STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", -1002728630709)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002547512665))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", )
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://cobrakaibgmi:RgHcjWur4SwZ3OtJ@cluster0.85qy8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "espdarename")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
 
 
 # Other Variables Config
